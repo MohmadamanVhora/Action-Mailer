c966e45ee6070fc84ffc23c95407e08f5b4be7e6
Mohmadaman Vhora <mohmadaman@vhora.com>
2023-06-12 20:09:55 +0530

    TE7-T132:Rails:Add rspec test cases for controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_and_belongs_to_many :events
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }, if: :password_digest_changed?
   validates :profile, presence: true
 
   private
